231d862a2477b84ca993b1eb59ebbdf5c4509f21
DESKTOP-P7P3MBD\Admin <vinod.patil377@gmail.com>
2022-07-25 14:58:19 +0530

    Updates

---
+++ precache-manifest.a86734b74a759cfac4ff2fcefd1dd77c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b17bbc6a695d02d5e314",
+    "url": "/torweld/static/css/main.271e167f.chunk.css"
+  },
+  {
+    "revision": "b17bbc6a695d02d5e314",
+    "url": "/torweld/static/js/main.b17bbc6a.chunk.js"
+  },
+  {
+    "revision": "ac9f703b20cde439a02d",
+    "url": "/torweld/static/css/1.97746016.chunk.css"
+  },
+  {
+    "revision": "ac9f703b20cde439a02d",
+    "url": "/torweld/static/js/1.ac9f703b.chunk.js"
+  },
+  {
+    "revision": "04bb7a33a8c3f9bd1d49",
+    "url": "/torweld/static/js/runtime~main.04bb7a33.js"
+  },
+  {
+    "revision": "0b1a6be86f420961beb3674885348b10",
+    "url": "/torweld/static/media/welcome.0b1a6be8.png"
+  },
+  {
+    "revision": "b345643bf16e34b914bf3992564eb6b7",
+    "url": "/torweld/static/media/about-img.b345643b.png"
+  },
+  {
+    "revision": "3d31f22bc797e56f5ed823d80963fc17",
+    "url": "/torweld/index.html"
+  }
+];
